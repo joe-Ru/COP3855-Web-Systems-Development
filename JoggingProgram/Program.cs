493db926ac1dc108c61622a275f23410ebe9263d
sigma0wl <ruizjoseph17@gmail.com>
2020-03-04 08:56:12 -0500

    Jogging Program and RealEstate Application

---
+++ JoggingProgram/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using static System.Console;
+
+namespace JoggingProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Declaring variables
+            int initialStrideCount, finalStrideCount, hrs, mins, totalMinutes;
+            double numberOfStepsPerMin, distanceTraveled;
+
+            const double STRIDE = 2.5;
+            const int FEET_PER_MILE = 5280;
+
+            DisplayInstructions();
+
+            initialStrideCount = GetNumberStrides("first");
+            finalStrideCount = GetNumberStrides("last");
+            InputJoggingTime(out hrs, out mins);
+
+            totalMinutes = CalculateTime(hrs, mins);
+            numberOfStepsPerMin = CalculateAvgSteps(initialStrideCount, finalStrideCount);
+            distanceTraveled = CalculateDistance(numberOfStepsPerMin, totalMinutes);
+
+            DisplayResults(numberOfStepsPerMin, mins, hrs, distanceTraveled);
+
+        }
+
+        public static void DisplayInstructions()
+        {
+            WriteLine("This program will calculate the distance travelled based on the inputs you insert");
+            WriteLine("The values entered will be integers and float values");
+        }
+
+        public static int GetNumberStrides(string when)
+        {
+            WriteLine("Enter the {0}, number of steps", when);
+            string inValue = ReadLine();
+            int numberOfSteps = int.Parse(inValue);
+            return numberOfSteps;
+        }
+        public static void InputJoggingTime(out int hrs, out int mins)
+        {
+            WriteLine("Enter number of hours");
+            string inValue = ReadLine();
+            hrs = int.Parse(inValue);
+
+            WriteLine("Enter number of minutes");
+            inValue = ReadLine();
+            mins = int.Parse(inValue);
+        }
+        public static double CalculateAvgSteps(int initialStrideCount, int finalStrideCount)
+        {
+            return (initialStrideCount + finalStrideCount) / 2.0;
+        }
+
+        public static int CalculateTime(int hrs, int mins)
+        {
+            return hrs * 60 + mins;
+        }
+        public static double CalculateDistance(double numberOfStepsPerMin, int totalMinutes)
+        {
+            return numberOfStepsPerMin * 2.5 * totalMinutes / 5280;
+        }
+        public static void DisplayResults(double numberOfStepsPerMin, int mins, int hrs, double distanceTraveled)
+        {
+            WriteLine("Strides Per Minutes: {0} Steps", numberOfStepsPerMin);
+            WriteLine("Jogging Time: {0} Hours {1} Minutes", hrs, mins);
+            WriteLine("Distance Traveled: {0} Miles", distanceTraveled);
+        }
+
+    }
+}
---
+++ RealEstateInvestmentActivity/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using static System.Console;
+
+namespace RealEstateInvestmentActivity
+{
+    class RealEstateInvestment
+    {
+        private int yearBuilt;
+        private double purchasePrice, incomeFromRent, monthlyExpense;
+        private string streetAddress;
+
+        public double MonthlyExpense
+        {
+            set
+            {
+                monthlyExpense = value;
+            }
+            get
+            {
+                return monthlyExpense;
+            }
+            
+
+        }
+        public double IncomeFromRent
+        {
+            
+            set
+            {
+                incomeFromRent = value;
+            }
+            get
+            {
+                return incomeFromRent;
+            }
+        }
+        public string StreetAddress
+        {
+            get
+            {
+                return streetAddress;
+            }
+        }
+        public int YearBuilt
+        {
+            get
+            {
+                return yearBuilt;
+            }
+        }
+        public double PurchasePrise
+        {
+            get
+            {
+                return purchasePrice;
+            }
+        }
+        public double DetermineMonthlyEarnings()
+        {
+            return (incomeFromRent - monthlyExpense);
+        }
+
+        public RealEstateInvestment(int year, double purchase, string street)
+        {
+            yearBuilt = year;
+            purchasePrice = purchase;
+            streetAddress = street;
+        }
+    }
+
+    class PropertyApp
+    {
+        
+        static void Main(string[] args)
+        {
+            const double RENTAL_AMOUNT = 1000.00;
+            RealEstateInvestment invest1 = new RealEstateInvestment(2004, 150000, "65th Street");
+
+            invest1.MonthlyExpense = GetExpenses();
+            invest1.IncomeFromRent = RENTAL_AMOUNT;
+
+            WriteLine("Property Location: " +  invest1.StreetAddress);
+            WriteLine("Earnings For Month: " + invest1.DetermineMonthlyEarnings());
+        }
+
+        public static double GetExpenses()
+        {
+            string inValue;
+            double insurance, taxes, utilities;
+
+            WriteLine("Enter the insurance rate: ");
+            inValue = ReadLine();
+            insurance = double.Parse(inValue);
+
+            WriteLine("Enter the taxes: ");
+            inValue = ReadLine();
+            taxes = double.Parse(inValue);
+
+            WriteLine("Enter utility charges: ");
+            inValue = ReadLine();
+            utilities = double.Parse(inValue);
+
+            return (insurance / 12 + taxes / 12 + utilities);
+        }
+    }
+}
